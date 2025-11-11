94f8b696de494f39474e5b0b6ce32e199de4aec9
msmithfl <msmithfl@icloud.com>
2025-11-10 21:51:44 -0500

    rm trailing slash

--- backend/FactoryWatch.Api/Program.cs
+++ backend/FactoryWatch.Api/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
         policy.WithOrigins(
             "http://localhost:5173",
             "https://localhost:5173",
-            "https://factory-watch-eam.vercel.app/"
+            "https://factory-watch-eam.vercel.app"
             )
             .AllowAnyHeader()
             .AllowAnyMethod();

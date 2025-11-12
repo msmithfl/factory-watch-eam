37f27e0b43ddefb147823446be44235e76f89634
msmithfl <msmithfl@icloud.com>
2025-11-12 14:14:52 -0500

    rm unused import

--- frontend/FactoryWatch.Client/src/components/Sidebar.tsx
+++ frontend/FactoryWatch.Client/src/components/Sidebar.tsx
@@ -1,6 +1,5 @@
 import { Link, useLocation } from 'react-router-dom'
 import { MdDashboard } from 'react-icons/md'
-import { FiSettings } from 'react-icons/fi'
 import { BsClipboardCheck } from 'react-icons/bs'
 import { FaTools } from "react-icons/fa";
 
